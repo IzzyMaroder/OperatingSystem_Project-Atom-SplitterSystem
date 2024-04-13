ab34fe599f8600867c0a1617b44dea28c10750c7
IzzyMaroder <leonardo.spinelli@edu.unito.it>
2024-04-13 19:23:47 +0200

    init_file tested and removed debug functions

--- Master.c
+++ Master.c
@@ -1,3 +1,7 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -12,37 +16,22 @@ int N_ATOMI_INIT;
 
 int input_file(char * pathname) {
 	int file = open(pathname, O_RDONLY);
+
 	if(file == -1) { 
 		perror("failed to open file!\n");
 		close(file);
 		exit(EXIT_FAILURE);
 	}
+
 	lseek(file, 14, SEEK_SET);
 	char * buf = malloc(1);
 	read(file, buf, 1);
 	ENERGY_DEMAND = atoi(buf);
-	/* START DEBUG */
-	if(ENERGY_DEMAND) {
-		printf("readed ENERGY_DEMAND %d\n", ENERGY_DEMAND);
-	} else {
-		fprintf(stderr,"Error: ENERGY_DEMAND is null.\n");
-		close(file);
-		exit(EXIT_FAILURE);
-	}
-	/* END DEBUG */
-
-	lseek(file, 14, SEEK_CUR);
+	
+	lseek(file, 28, SEEK_CUR);
 	read(file, buf, 1);
 	N_ATOMI_INIT = atoi(buf);
-	/* START DEBUG */
-	if(N_ATOMI_INIT) {
-		printf("readed N_ATOMI_INIT %d\n", N_ATOMI_INIT);
-	} else {
-		fprintf(stderr, "Error: N_ATOMI_INIT is null.\n");
-		close(file);
-		exit(EXIT_FAILURE);
-	}
-	/* END DEBUG */
+	
 	close(file);
 }
 
