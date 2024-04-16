201166f82a279cb8047ae8b60e50a4ea1ca78995
IzzyMaroder <leonardo.spinelli@edu.unito.it>
2024-04-16 21:08:23 +0200

    added print_mem and clear_mem. initialized struct elem in mem_init function

--- sharedmemory.c
+++ sharedmemory.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "sharedmemory.h"
 
 struct shm* mem_init() {
     struct shm * datap;
@@ -17,6 +18,41 @@ struct shm* mem_init() {
         exit(EXIT_FAILURE);
     }
 
+    shmctl(memId, IPC_RMID, NULL);
+
+    datap->CURRENT_ENERGY = 0;
+
+    datap->TOT_ACTIVATIONS = 0;
+    datap->ACTIVATIONS_PER_SEC = 0;
+    
+    datap->TOT_SCISSIONS = 0;
+    datap->SCISSIONS_PER_SEC = 0;
+    
+    datap->TOT_PRODUCED_ENERGY = 0;
+    datap->PRODUCED_ENERGY_PER_SEC = 0;
+    
+    datap->TOT_CONSUMED_ENERGY = 0;
+    datap->CONSUMED_ENERGY_PER_SEC = 0;
+    
+    datap->TOT_SCORIE = 0;
+    datap->SCORIE_PER_SEC = 0;
+    
+    datap->ABSORBED_ENERGY = 0;
+    datap->memId = memId;
+    
     printf("Shared memory passed all init phase.\n");
 
+    return datap;
+
+}
+
+void print_mem(struct shm * datap) {
+    datap->TOT_ACTIVATIONS = datap->TOT_ACTIVATIONS+ datap->ACTIVATIONS_PER_SEC;
+
+    printf("Total activations: %d\n", datap->TOT_ACTIVATIONS);
+
+}
+
+void clean_mem(struct shm * datap) {
+    shmdt(datap);
 }
