969bb89fd0e67d674203d56ba122afe5afbfe78b
IzzyMaroder <leonardo.spinelli@edu.unito.it>
2024-04-16 21:05:55 +0200

    added struct shm and functions

--- sharedmemory.h
+++ sharedmemory.h
@@ -1,6 +1,25 @@
+struct shm {
+    int CURRENT_ENERGY;
+
+    int TOT_ACTIVATIONS;
+    int ACTIVATIONS_PER_SEC;
+
+    int TOT_SCISSIONS;
+    int SCISSIONS_PER_SEC;
+
+    int TOT_PRODUCED_ENERGY;
+    int PRODUCED_ENERGY_PER_SEC;
+
+    int TOT_CONSUMED_ENERGY;
+    int CONSUMED_ENERGY_PER_SEC;
 
 
+    int TOT_SCORIE;
+    int SCORIE_PER_SEC;
+    int ABSORBED_ENERGY;
+    int memId;
+};
 
-
-void print_memory();
-void clean_memory();
+struct shm* mem_init();
+void print_mem(struct shm * datap);
+void clean_mem(struct shm * datap);
