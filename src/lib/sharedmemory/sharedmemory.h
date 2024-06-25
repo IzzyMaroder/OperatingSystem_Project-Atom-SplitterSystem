47d3e5edd578088085c106c23e4b2992bce79dff
IzzyMaroder <leonardo.spinelli@edu.unito.it>
2024-06-25 21:22:49 +0200

    change energy_absorbed type

--- src/lib/sharedmemory/sharedmemory.h
+++ src/lib/sharedmemory/sharedmemory.h
@@ -21,9 +21,9 @@ struct shmConf {
 
 struct shmStat {
     int energy_produced, num_scories, num_scissions, 
-    num_activations, energy_consumed, energy_absorbed, n_ofatoms, flags, stop_inhi;
+    num_activations, energy_consumed, n_ofatoms, flags, stop_inhi;
 
-    double energy_to_remove;
+    double energy_to_remove, energy_absorbed;
 };
 
 struct shm {
